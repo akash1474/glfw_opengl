92c13b0d315a5a164c7f163950919a20c82d1612
Akash Pandit <panditakash38@gmail.com>
2022-09-14 19:08:14 +0530

    switched from glad->glew

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
-CXXFLAGS=-O3 -s -I./include -L./bin -lglfw3 -lopengl32
+CXXFLAGS=-O3 -s -I./include -L./bin -lglfw3 -lopengl32 -lglew32
 SRC_DIR:=$(wildcard ./src/*.cpp)
 SRC_OBJ:=$(patsubst %.cpp,%.o,$(SRC_DIR))
 SRC_DEP:=$(patsubst %.cpp,%.d,$(SRC_DIR))
 
-LIB_DIR:=$(wildcard ./lib/*.cpp)
-LIB_OBJ:=$(patsubst %.cpp,%.o,$(LIB_DIR))
+# LIB_DIR:=$(wildcard ./lib/*.cpp)
+# LIB_OBJ:=$(patsubst %.cpp,%.o,$(LIB_DIR))
 
-all:$(LIB_OBJ) $(SRC_OBJ)
+all:$(SRC_OBJ)
 	@echo main.exe
-	@g++ $(SRC_OBJ) $(LIB_OBJ) $(CXXFLAGS) -o ./bin/main && ./bin/main.exe
+	@g++ $(SRC_OBJ) $(CXXFLAGS) -o ./bin/main && ./bin/main.exe
 
 %.o:%.cpp
 	@echo $<
--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,50 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "iostream"
 
-int main()
+int main(void)
 {
     GLFWwindow* window;
-    // Initialize GLFW
+
+    /* Initialize the library */
     if (!glfwInit())
-        exit(EXIT_FAILURE);
+        return -1;
 
 
-
-    // Tell GLFW what version of OpenGL we are using 
-    // In this case we are using OpenGL 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    int WIDTH=400;
-    int HEIGHT=400;
-
-
-    // Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-    window = glfwCreateWindow(WIDTH, HEIGHT, "YoutubeOpenGL", NULL, NULL);
-    // Error check if the window fails to create
-    if (window == NULL)
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    // Introduce the window into the current context
+
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if(glewInit()!=GLEW_OK) std::cout << "Error" << std::endl;
 
 
-    gladLoadGL();
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
-    glViewport(0, 0, WIDTH, HEIGHT);
-    glOrtho(0, WIDTH, 0, HEIGHT, 0,1);
-
-    GLfloat lineVert[]={
-        100.0f,100.0f,0.0f,
-        200.f,200.f,0.0,
-        300.f,100.f,0.0,
-        400.f,200.f,0.0
-    };
-
-
-
-
-    // Main while loop
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        // Specify the color of the background
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        // Clean the back buffer and assign the new color to it
         glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f,-0.5f);
+        glVertex2f(0.0f,0.5f);
+        glVertex2f(0.5f,-0.5f);
+        glEnd();
 
 
-        glEnable(GL_LINE_SMOOTH);
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3,GL_FLOAT,0,lineVert);
-        glDrawArrays(GL_LINES,0,4);
-        glDisableClientState(GL_VERTEX_ARRAY);
-        glDisable(GL_LINE_SMOOTH);
 
-        // Swap the back buffer with the front buffer
+
+
+
         glfwSwapBuffers(window);
-
-        // Take care of all GLFW events
         glfwPollEvents();
     }
 
-
-
-    // Delete window before ending the program
-    glfwDestroyWindow(window);
-    // Terminate GLFW before ending the program
     glfwTerminate();
     return 0;
 }
