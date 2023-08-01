69fe0a1ddd8493a1d56e9f225f11c5e7b99099be
Akash Pandit <panditakash38@gmail.com>
2023-08-01 19:40:04 +0530

    setup script added

--- setup.bat
+++ setup.bat
@@ -3,27 +3,48 @@ set glfw=https://github.com/glfw/glfw.git
 set spdlog=-o spdlog.zip https://codeload.github.com/gabime/spdlog/zip/refs/tags/v1.12.0
 set imgui=-o imgui.zip https://codeload.github.com/ocornut/imgui/zip/refs/tags/v1.89.7
 
-git clone %glfw%
+
+echo -- Cloning glfw github repo
+git clone --depth 1 %glfw%
+echo.
+echo -- Downloading spdlog
 curl %spdlog%
+echo.
+echo -- Downloading imgui
 curl %imgui%
+
+
+
 if exist glfw (
-	move ./glfw/* ./packages/glfw
-	del glfw
+	echo.
+	echo Setting Up GLFW
+	mv --force ./glfw/* ./packages/glfw
 ) else (
 	echo [ GLFW ] No directory found
 	exit /b
 )
-echo Extracting spdlog.zip to spdlog-1.12.0
-winrar x spdlog.zip *
-move ./spdlog-1.12.0/* ./packages/spdlog
-del spdlog-1.12.0
+echo.
+echo -- Extracting Files
+winrar x -idv spdlog.zip *
+winrar x -idv imgui.zip *
 
-echo Extracting imgui.zip to imgui-1.89.7
-winrar x imgui.zip *
-move ./imgui-1.89.7/*.cpp ./packages/imgui
-move ./imgui-1.89.7/*.h ./packages/imgui
-move ./imgui-1.89.7/backends/imgui_impl_opengl2.cpp ./packages/imgui
-move ./imgui-1.89.7/backends/imgui_impl_opengl2.h ./packages/imgui
-move ./imgui-1.89.7/backends/imgui_impl_glfw.cpp ./packages/imgui
-move ./imgui-1.89.7/backends/imgui_impl_glfw.h ./packages/imgui
-del imgui-1.89.7
+if not exist .\packages\spdlog mkdir .\packages\spdlog
+if not exist .\packages\imgui mkdir .\packages\imgui
+
+echo -- Setting up spdlog
+mv -f ./spdlog-1.12.0/* ./packages/spdlog
+
+echo -- Setting up imgui
+mv -f ./imgui-1.89.7/*.cpp ./packages/imgui
+mv -f ./imgui-1.89.7/*.h ./packages/imgui
+mv -f ./imgui-1.89.7/backends/imgui_impl_opengl2.cpp ./packages/imgui
+mv -f ./imgui-1.89.7/backends/imgui_impl_opengl2.h ./packages/imgui
+mv -f ./imgui-1.89.7/backends/imgui_impl_glfw.cpp ./packages/imgui
+mv -f ./imgui-1.89.7/backends/imgui_impl_glfw.h ./packages/imgui
+
+echo -- Cleaning
+rmdir /s /q imgui-1.89.7
+del /f imgui.zip
+del /f spdlog.zip
+rmdir /s /q spdlog-1.12.0
+rmdir /s /q glfw
